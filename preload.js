eab7dc2018c08d2f7e3d03c3114473d12b053c75
Chiara <34109996+chiaramooney@users.noreply.github.com>
2025-10-01 11:29:12 -0700

    Try External Addon, Code Cleanup

--- preload.js
+++ preload.js
@@ -4,6 +4,7 @@ const { contextBridge } = require('electron');
 const myAddon = require('./myAddon/build/Release/myAddon.node');
 const phiSilicaAddon = require('./PhiSilicaAddon/build/Release/PhiSilicaAddon.node');
 const windowsaiAddon = require('./WindowsAIAddon/build/Release/WindowsAIAddon.node');
+const {LanguageModel, AIFeatureReadyState} = require('../electron-windows-ai-addon/windows-ai-addon/build/Release/windows-ai-addon.node');
 
 contextBridge.exposeInMainWorld('winAppSdk', {
   showNotification: (title, body) => {
@@ -33,5 +34,20 @@ contextBridge.exposeInMainWorld('windowsAI', {
   },
   generateCaption: (filePath) => {
     return windowsaiAddon.generateCaption(filePath);
+  }
+});
+
+contextBridge.exposeInMainWorld('externalWindowsAI', {
+  generateText: (prompt) => {
+    var readyState = LanguageModel.GetReadyState();
+    if (readyState == AIFeatureReadyState.NotReady) {
+      LanguageModel.EnsureReadyAsync();
+    } else if (readyState == AIFeatureReadyState.Ready) {
+      var languageModel = LanguageModel.CreateAsync();
+      if (languageModel){
+        return languageModel.GenerateResponseAsync(prompt);
+      }
+    }
+    return "Model not in a ready state."
   }
 });
