bb9f657c594f472bf87313072a2742d97cb2bfff
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2025-12-10 16:15:59 -0800

    Add x64 Support

--- forge.config.js
+++ forge.config.js
@@ -4,6 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    arch: ['x64', 'arm64'],
   },
   rebuildConfig: {},
   makers: [
